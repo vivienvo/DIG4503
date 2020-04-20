167eab746e564dd720349e7bd607959c782205e4
vivienvo <42749976+vivienvo@users.noreply.github.com>
2020-04-19 22:20:21 -0400

    docs upload

---
+++ docs/precache-manifest.475f2a840c2d2b83338647578e418646.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bba9ed54b37643f483779d3a59d5b5e1",
+    "url": "/DIG4503/index.html"
+  },
+  {
+    "revision": "71ffb35d3af50c647338",
+    "url": "/DIG4503/static/css/main.590a5ded.chunk.css"
+  },
+  {
+    "revision": "838bf73ece3181f37b27",
+    "url": "/DIG4503/static/js/2.e1c407ad.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/DIG4503/static/js/2.e1c407ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "71ffb35d3af50c647338",
+    "url": "/DIG4503/static/js/main.e279a24e.chunk.js"
+  },
+  {
+    "revision": "48400eceb97398868800",
+    "url": "/DIG4503/static/js/runtime-main.01b8c0f6.js"
+  }
+]);
